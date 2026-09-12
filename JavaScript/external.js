86ddd07e16aefa8d88580ab047b40219259421ab
Mohsin Khan <mohsin.kp02@gmail.com>
2026-09-12 15:04:16 +0530

    update

---
+++ JavaScript/external.js
@@ -0,0 +1,19 @@
+document.write("im from external.js file");
+console.log("im from console");
+
+document.write("Hello");
+document.write("<br>");
+document.write("world");
+document.write("<h1>Heading<h1>");
+document.write("<ul><li>Mango</li><li>Apple</li></ul>");
+document.write(`<table border="5">
+    < tr >
+        <td>john</td>
+        <td>101</td>
+    </tr >
+    <tr>
+        <td>Smith</td>
+        <td>102</td>
+    </tr>
+        
+    </table >`);
